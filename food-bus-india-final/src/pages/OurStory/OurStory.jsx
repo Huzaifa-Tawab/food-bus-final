9eb43f49e998a1b02731d73cf2a4f1a5dcceb4a4
Fasih <Fasih@softxion.com>
2024-01-06 18:18:49 +0500

    images

--- food-bus-india-final/src/pages/OurStory/OurStory.jsx
+++ food-bus-india-final/src/pages/OurStory/OurStory.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+import './ourstory.css'
 function OurStory() {
-  return <div>OurStory</div>;
+  return (
+    <div className="OurStory">
+      <div className="OurStory-hero"> 
+      
+      </div>    
+      <div className="OurStory-content"> 
+        <div className="OurStory-content-card">
+
+        </div>
+        <div className="OurStory-content-card reverse">
+          
+          </div>
+      </div>
+    </div>
+  )
 }
 
 export default OurStory;
