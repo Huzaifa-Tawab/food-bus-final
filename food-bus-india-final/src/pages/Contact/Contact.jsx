26850514c2aff9d354760e7e321ea491b08e4b51
Fasih <Fasih@softxion.com>
2024-01-09 14:39:11 +0500

    contact page done and responsive

--- food-bus-india-final/src/pages/Contact/Contact.jsx
+++ food-bus-india-final/src/pages/Contact/Contact.jsx
@@ -1,7 +1,27 @@
 import React from "react";
-
+import "./contact.css";
+import mail from "./emailimg.png"
+import phone from "./phoneimg.png"
 function Contact() {
-  return <div>Contact</div>;
+  return(
+  <div className="contctback">
+   <div className="contct-content-box">
+    <h1>Got Suggestions ?  <br></br>We Here ya...</h1>
+    <div className="cntct-text">
+      <div className="contct-email">
+        <img src={mail}></img>
+        <h2>hello@fbi.com</h2>
+      </div>
+      <div className="phone-email">
+        <img src={phone}></img>
+        <h2>000000000000</h2>
+      </div>
+    </div>
+   </div>
+   <div clas></div>
+  </div>
+
+)
 }
 
 export default Contact;
