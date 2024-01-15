308c4e93be97afd9ad5d9d746d65e65977fb97b4
Fasih <Fasih@softxion.com>
2024-01-15 16:56:46 +0500

    allignmnt boss section

--- food-bus-india-final/src/components/Kitchen/Kitchen.jsx
+++ food-bus-india-final/src/components/Kitchen/Kitchen.jsx
@@ -20,23 +20,23 @@ function Kitchen() {
       </div>
       <div className="Kitchen-facts">
         <div className="Kitchen-fact blue">
-          <h1>Customers Served:</h1>
+          <h1>Customers Served</h1>
           <p>Over 193,000 customers have been served at Food Bus of India since our inception.</p>
         </div>
         <div className="Kitchen-fact red">
-          <h1> Local Ingredients:</h1>
+          <h1> Local Ingredients</h1>
           <p>
           70% of our ingredients are locally sourced, supporting community farmers and ensuring freshness.
           </p>
         </div>
         <div className="Kitchen-fact blue">
-          <h1>Team Members:</h1>
+          <h1>Team Members</h1>
           <p>
           Our dedicated team comprises over 70 talented chefs and staff committed to delivering exceptional dining experiences.
           </p>
         </div>
         <div className="Kitchen-fact red">
-          <h1>Cuisine Diversity: </h1>
+          <h1>Cuisine Diversity </h1>
           <p>
           Offering a menu featuring dishes from different regions of India & worldwide. Its Handy , Trendy and Affordable
           </p>
