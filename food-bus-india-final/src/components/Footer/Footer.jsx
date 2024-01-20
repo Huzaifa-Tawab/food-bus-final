d1c9b37ec134073939cdc1147dda5711678eb9ea
Huzaifa <huzaifatawab33@gmail.com>
2024-01-20 17:51:00 +0500

    #

--- food-bus-india-final/src/components/Footer/Footer.jsx
+++ food-bus-india-final/src/components/Footer/Footer.jsx
@@ -18,7 +18,6 @@ function Footer() {
           <button>Contact Us</button>
         </div> */}
         <div className="Footer-Middle">
-          <div className="Footer-Middle-Logo"></div>
           <div className="Footer-Middle-About">
             <img src={Logo} alt="Logo" />
             {/* <h3>About Us.</h3> */}
@@ -30,38 +29,49 @@ function Footer() {
           </div>
           <div className="Footer-Middle-UseFull">
             <h3>Useful Links</h3>
+            <div className="links-list">
+              <Link to={"/"}>
+                <p className="points">Home</p>
+              </Link>
 
-            <Link to={"/"}>
-              <p className="points">Home</p>
-            </Link>
+              <Link to={"/ourstory"}>
+                <p className="points">Our Story </p>
+              </Link>
 
-            <Link to={"/ourstory"}>
-              <p className="points">Our Story </p>
-            </Link>
-
-            <Link to={"/franchise"}>
-              <p className="points">Franchise</p>
-            </Link>
-            <Link to={"/menu"}>
-              <p className="points">Menu</p>
-            </Link>
-            <Link to={"/faq"}>
-              <p className="points">Faq </p>
-            </Link>
-            <Link to={"/contact"}>
-              <p className="points">Contact</p>
-            </Link>
+              <Link to={"/franchise"}>
+                <p className="points">Franchise</p>
+              </Link>
+              <Link to={"/menu"}>
+                <p className="points">Menu</p>
+              </Link>
+              <Link to={"/faq"}>
+                <p className="points">Faq </p>
+              </Link>
+              <Link to={"/contact"}>
+                <p className="points">Contact</p>
+              </Link>
+            </div>
           </div>
           <div className="Footer-Middle-Help">
-            <input type="text" placeholder="Email" />
-            <input type="number" placeholder="Phone Number" />
+            <h3>
+              Connect With Us To Know Stay <br />
+              Update!!!
+            </h3>
+            <input type="email" value={"Email"} />
+            <input type="number" value={"1234567890"} />
             <button>Submit</button>
           </div>
         </div>
       </div>
-      {/* <div className="Footer-Bottom">
-        <p>Copyright © 2023. All Rights Reserved.</p>
-      </div> */}
+      <div className="Footer-Bottom">
+        <p>
+          Outlets : Gate no. 2, Rajendra Place Metro, Delhi 110008 , Gate No 5,
+          Lajpat Nagar Metro Station, Block C, Lajpat Nagar III, Lajpat Nagar,
+          New Delhi, Delhi 110024 ,Food Bus of India Vishwavidyalaya Metro
+          Station Gate No 04,Delhi, New Delhi, Delhi 110007 , Gate no 4, metro
+          station, South Extension, 2, Delhi, New Delhi, Delhi 110049
+        </p>
+      </div>
     </>
   );
 }
