610dbb7079d48ceb2865d876ff11c5443486f8c8
Huzaifa <huzaifatawab33@gmail.com>
2024-01-22 22:59:41 +0500

    menu update

--- food-bus-india-final/src/pages/Menu/Menu.jsx
+++ food-bus-india-final/src/pages/Menu/Menu.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 import "./menu.css";
-import DesktopMenu from "../../assets/menu/men.png";
-import MobileMenu1 from "../../assets/menu/Menu-M1.png";
-import MobileMenu2 from "../../assets/menu/Menu-M2.png";
-import MobileMenu3 from "../../assets/menu/Menu-M3.png";
+import Menu1 from "./Page1.png";
+import Menu2 from "./Page2.png";
+import Menu3 from "./Page3.png";
 
 function Menu() {
   return (
     <div className="Menu">
-      <div className="Menu-desktop">
-        <img src={DesktopMenu} alt="Desktop Menu" />
-      </div>
       <div className="Menu-mobile">
         <div className="Menu-mobile-alltime">
-          <img src={MobileMenu1} alt="Mobile Menu" />
+          <img src={Menu1} alt="Mobile Menu" />
         </div>
         <div className="Menu-mobile-extra">
-          <img src={MobileMenu2} alt="Mobile Menu" />
+          <img src={Menu2} alt="Mobile Menu" />
         </div>
         <div className="Menu-mobile-bevrages">
-          <img src={MobileMenu3} alt="Mobile Menu" />
+          <img src={Menu3} alt="Mobile Menu" />
         </div>
       </div>
     </div>
